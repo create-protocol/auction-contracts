4216147c7de36a7fe90d7b35cb8c5285415a24fd
chirag-bgh <chirag2bsingh@gmail.com>
2022-12-09 21:44:17 +0530

    feat: add Transfer Helpers

---
+++ src/helpers/TransferHelper.sol
@@ -0,0 +1,254 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.17;
+
+import "./TransferConstants.sol";
+import "./TransferErrors.sol";
+
+contract TransferHelper {
+    
+    function _erc721Transfer(
+        address token,
+        address from,
+        address to,
+        uint256 id
+    ) internal {
+
+        assembly {
+            // If the token has no code, revert.
+            if iszero(extcodesize(token)) {
+                mstore(NoContract_error_sig_ptr, NoContract_error_signature)
+                mstore(NoContract_error_token_ptr, token)
+                revert(NoContract_error_sig_ptr, NoContract_error_length)
+            }
+
+            // The free memory pointer memory slot will be used when populating
+            // call data for the transfer; read the value and restore it later.
+            let memPointer := mload(FreeMemoryPointerSlot)
+
+            // Write call data to memory starting with function selector.
+            mstore(ERC721_transferFrom_sig_ptr, ERC721_transferFrom_signature)
+            mstore(ERC721_transferFrom_from_ptr, from)
+            mstore(ERC721_transferFrom_to_ptr, to)
+            mstore(ERC721_transferFrom_id_ptr, id)
+
+            // Perform the call, ignoring return data.
+            let success := call(
+                gas(),
+                token,
+                0,
+                ERC721_transferFrom_sig_ptr,
+                ERC721_transferFrom_length,
+                0,
+                0
+            )
+
+            // If the transfer reverted:
+            if iszero(success) {
+                // If it returned a message, bubble it up as long as sufficient
+                // gas remains to do so:
+                if returndatasize() {
+                    // Ensure that sufficient gas is available to copy
+                    // returndata while expanding memory where necessary. Start
+                    // by computing word size of returndata & allocated memory.
+                    // Round up to the nearest full word.
+                    let returnDataWords := div(
+                        add(returndatasize(), AlmostOneWord),
+                        OneWord
+                    )
+
+                    // Note: use the free memory pointer in place of msize() to
+                    // work around a Yul warning that prevents accessing msize
+                    // directly when the IR pipeline is activated.
+                    let msizeWords := div(memPointer, OneWord)
+
+                    // Next, compute the cost of the returndatacopy.
+                    let cost := mul(CostPerWord, returnDataWords)
+
+                    // Then, compute cost of new memory allocation.
+                    if gt(returnDataWords, msizeWords) {
+                        cost := add(
+                            cost,
+                            add(
+                                mul(
+                                    sub(returnDataWords, msizeWords),
+                                    CostPerWord
+                                ),
+                                div(
+                                    sub(
+                                        mul(returnDataWords, returnDataWords),
+                                        mul(msizeWords, msizeWords)
+                                    ),
+                                    MemoryExpansionCoefficient
+                                )
+                            )
+                        )
+                    }
+
+                    // Finally, add a small constant and compare to gas
+                    // remaining; bubble up the revert data if enough gas is
+                    // still available.
+                    if lt(add(cost, ExtraGasBuffer), gas()) {
+                        // Copy returndata to memory; overwrite existing memory.
+                        returndatacopy(0, 0, returndatasize())
+
+                        // Revert, giving memory region with copied returndata.
+                        revert(0, returndatasize())
+                    }
+                }
+
+                // Otherwise revert with a generic error message.
+                mstore(
+                    TokenTransferGenericFailure_error_sig_ptr,
+                    TokenTransferGenericFailure_error_signature
+                )
+                mstore(TokenTransferGenericFailure_error_token_ptr, token)
+                mstore(TokenTransferGenericFailure_error_from_ptr, from)
+                mstore(TokenTransferGenericFailure_error_to_ptr, to)
+                mstore(TokenTransferGenericFailure_error_id_ptr, id)
+                mstore(TokenTransferGenericFailure_error_amount_ptr, 1)
+                revert(
+                    TokenTransferGenericFailure_error_sig_ptr,
+                    TokenTransferGenericFailure_error_length
+                )
+            }
+
+            // Restore the original free memory pointer.
+            mstore(FreeMemoryPointerSlot, memPointer)
+
+            // Restore the zero slot to zero.
+            mstore(ZeroSlot, 0)
+        }
+    }
+
+    function _erc1155Transfer(
+        address token,
+        address from,
+        address to,
+        uint256 id,
+        uint256 amount
+    ) internal {
+        // Utilize assembly to perform an optimized ERC1155 token transfer.
+        assembly {
+            // If the token has no code, revert.
+            if iszero(extcodesize(token)) {
+                mstore(NoContract_error_sig_ptr, NoContract_error_signature)
+                mstore(NoContract_error_token_ptr, token)
+                revert(NoContract_error_sig_ptr, NoContract_error_length)
+            }
+
+            // The following memory slots will be used when populating call data
+            // for the transfer; read the values and restore them later.
+            let memPointer := mload(FreeMemoryPointerSlot)
+            let slot0x80 := mload(Slot0x80)
+            let slot0xA0 := mload(Slot0xA0)
+            let slot0xC0 := mload(Slot0xC0)
+
+            // Write call data into memory, beginning with function selector.
+            mstore(
+                ERC1155_safeTransferFrom_sig_ptr,
+                ERC1155_safeTransferFrom_signature
+            )
+            mstore(ERC1155_safeTransferFrom_from_ptr, from)
+            mstore(ERC1155_safeTransferFrom_to_ptr, to)
+            mstore(ERC1155_safeTransferFrom_id_ptr, id)
+            mstore(ERC1155_safeTransferFrom_amount_ptr, amount)
+            mstore(
+                ERC1155_safeTransferFrom_data_offset_ptr,
+                ERC1155_safeTransferFrom_data_length_offset
+            )
+            mstore(ERC1155_safeTransferFrom_data_length_ptr, 0)
+
+            // Perform the call, ignoring return data.
+            let success := call(
+                gas(),
+                token,
+                0,
+                ERC1155_safeTransferFrom_sig_ptr,
+                ERC1155_safeTransferFrom_length,
+                0,
+                0
+            )
+
+            // If the transfer reverted:
+            if iszero(success) {
+                // If it returned a message, bubble it up as long as sufficient
+                // gas remains to do so:
+                if returndatasize() {
+                    // Ensure that sufficient gas is available to copy
+                    // returndata while expanding memory where necessary. Start
+                    // by computing word size of returndata & allocated memory.
+                    // Round up to the nearest full word.
+                    let returnDataWords := div(
+                        add(returndatasize(), AlmostOneWord),
+                        OneWord
+                    )
+
+                    // Note: use the free memory pointer in place of msize() to
+                    // work around a Yul warning that prevents accessing msize
+                    // directly when the IR pipeline is activated.
+                    let msizeWords := div(memPointer, OneWord)
+
+                    // Next, compute the cost of the returndatacopy.
+                    let cost := mul(CostPerWord, returnDataWords)
+
+                    // Then, compute cost of new memory allocation.
+                    if gt(returnDataWords, msizeWords) {
+                        cost := add(
+                            cost,
+                            add(
+                                mul(
+                                    sub(returnDataWords, msizeWords),
+                                    CostPerWord
+                                ),
+                                div(
+                                    sub(
+                                        mul(returnDataWords, returnDataWords),
+                                        mul(msizeWords, msizeWords)
+                                    ),
+                                    MemoryExpansionCoefficient
+                                )
+                            )
+                        )
+                    }
+
+                    // Finally, add a small constant and compare to gas
+                    // remaining; bubble up the revert data if enough gas is
+                    // still available.
+                    if lt(add(cost, ExtraGasBuffer), gas()) {
+                        // Copy returndata to memory; overwrite existing memory.
+                        returndatacopy(0, 0, returndatasize())
+
+                        // Revert, giving memory region with copied returndata.
+                        revert(0, returndatasize())
+                    }
+                }
+
+                // Otherwise revert with a generic error message.
+                mstore(
+                    TokenTransferGenericFailure_error_sig_ptr,
+                    TokenTransferGenericFailure_error_signature
+                )
+                mstore(TokenTransferGenericFailure_error_token_ptr, token)
+                mstore(TokenTransferGenericFailure_error_from_ptr, from)
+                mstore(TokenTransferGenericFailure_error_to_ptr, to)
+                mstore(TokenTransferGenericFailure_error_id_ptr, id)
+                mstore(TokenTransferGenericFailure_error_amount_ptr, amount)
+                revert(
+                    TokenTransferGenericFailure_error_sig_ptr,
+                    TokenTransferGenericFailure_error_length
+                )
+            }
+
+            mstore(Slot0x80, slot0x80) // Restore slot 0x80.
+            mstore(Slot0xA0, slot0xA0) // Restore slot 0xA0.
+            mstore(Slot0xC0, slot0xC0) // Restore slot 0xC0.
+
+            // Restore the original free memory pointer.
+            mstore(FreeMemoryPointerSlot, memPointer)
+
+            // Restore the zero slot to zero.
+            mstore(ZeroSlot, 0)
+        }
+    }
+}
